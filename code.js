3b4612f543d74365f4e02f7dab64399b3dce6ef7
Krishna Regmi <kisna72@gmail.com>
2019-10-07 17:28:55 -0500

    fist stab

--- code.js
+++ code.js
@@ -57,8 +57,8 @@ function getMakeSpan(ganttChartSolution){
 /**
  * Given a 1D solution, convert it into Gantt Chart Solution. 
  */
-function JSSP1dToGantt(jobInstance){
-    const test1D = [0,2,1,0,3,1,0,1,2,3,2,1,1,2,3,0,2,0,3,3]
+function JSSP1dToGantt(jobInstance, test1D){
+    //const test1D = [0,2,1,0,3,1,0,1,2,3,2,1,1,2,3,0,2,0,3,3]
     // const test1D =[0,0,0,0,0,1,1,1,1,1,2,2,2,2,2,3,3,3,3,3]
     const m = 5 // Number of Machines: 0,1,2,3,4,5
     const n = 4 // Number of Jobs. 0,1,2,3 -> as denoted in the 1d strucutre. 
@@ -66,7 +66,7 @@ function JSSP1dToGantt(jobInstance){
     for(let i = 0; i<m; i++){
         ganttChart.push([])
     }
-    console.log(ganttChart)
+    //console.log(ganttChart)
 
     // Next we fill the ganttChart with data based on test1D
     // We need access to the job instance so that we can look at
@@ -98,10 +98,73 @@ function JSSP1dToGantt(jobInstance){
         ganttChart[machine] = [...ganttChart[machine], ...ganttSchedule]
     })
 
-    console.log(ganttChart)
+    //console.log(ganttChart)
 
 
     return ganttChart
 }
-console.log("1d to gantt")
-JSSP1dToGantt(a,'')
+/**
+ * Generating MakeSpan is basically calculating Max of the end index of all times from all machines
+ * @param {*} ganttChart 
+ */
+function getMakeSpan(ganttChart){
+    return Math.max(...ganttChart.map(machineInfo => machineInfo[machineInfo.length -1 ]));
+}
+
+function FishesYatesShuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+}
+  
+
+/**
+ * Our Random Algorithm will 
+ */
+console.log("running optimization algo")
+function runOptimizationAlgo(m,n){
+    const startingInput = [0,0,0,0,0,1,1,1,1,1,2,2,2,2,2,3,3,3,3,3]
+    let gantt = []
+    let makeSpan = Infinity
+    const times = 100
+    for(let i = 0; i < times; i ++){
+        // Create new Job Shop Instance every time. 
+        const a = new JSSPProblemInstance(4,5)
+        a.jobs.push([0,10,1,20,2,20,3,40,4,10])
+        a.jobs.push([1,20,0,10,3,30,2,50,4,30])
+        a.jobs.push([2,30,1,20,4,12,3,40,0,10])
+        a.jobs.push([4,50,3,30,2,15,0,20,1,15])
+        //console.log(a)
+        //console.log("running once")
+        const randomizedInput = FishesYatesShuffle(startingInput)
+        //console.log("randomized input " , randomizedInput)
+        const gantFromRandInput = JSSP1dToGantt(a, randomizedInput);
+        const newMakeSpan = getMakeSpan(gantFromRandInput);
+        if(newMakeSpan < makeSpan){
+            makeSpan = newMakeSpan
+            gantt = gantFromRandInput
+            console.log("Found Better")
+            console.log(gantt)
+            console.log("============")
+        }
+    }
+    return gantt
+    console.log(gantt)
+}
+
+const best = runOptimizationAlgo()
+console.log("Best Gantt Chart is ")
+console.log(best)
