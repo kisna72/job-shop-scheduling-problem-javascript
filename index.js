e0854336e70db0d3d5006b1670b80abb8a117e3c
Krishna Regmi <kisna72@gmail.com>
2019-10-08 22:49:56 -0500

    Code to end if algo takes too long

--- index.js
+++ index.js
@@ -5,29 +5,28 @@ const generateRandom1D = require('./definitions/helpers');
 
 // Constants that can be changed. Problem is defined in a problem definition file.
 const PROBLEM_INSTANCE_FILE = "demo.txt"
-const ALGORITHM_REPETITION = 1000
-const ALGORITHM_MAX_TIME_MINS = 3 //Max time we want the algo to run.
+const ALGORITHM_REPETITION = 1000000
+const ALGORITHM_MAX_TIME_SECONDS = 1  //Max time we want the algo to run. TODO use timelimit
 
 // Step 1: Create a new instance of JSSPProblemInstance from file.
 const problem = new JSSPProblemInstance() // Instantiate with no data. 
 problem.generateJSSPFromTextFile(PROBLEM_INSTANCE_FILE)
-console.log(problem)
+// console.log(problem)
 // Step 2: Next, we run the algorithm for a set number of times.
-console.log("running optimization algo")
+// console.log("running optimization algo")
 function runOptimizationAlgo(problem){
     let gantt = []
     let makeSpan = Infinity
     // const times = 100
+    const algoStartTime = (new Date).getTime();
+    const algoMaxEndTime = algoStartTime + (ALGORITHM_MAX_TIME_SECONDS * 1000)
     for(let i = 0; i < ALGORITHM_REPETITION; i ++){
-        
-        // Create new Job Shop Instance every time. 
-        // const a = new JSSPProblemInstance(4,5)
-        // a.jobs.push([0,10,1,20,2,20,3,40,4,10])
-        // a.jobs.push([1,20,0,10,3,30,2,50,4,30])
-        // a.jobs.push([2,30,1,20,4,12,3,40,0,10])
-        // a.jobs.push([4,50,3,30,2,15,0,20,1,15])
-        // //console.log(a)
-        // //console.log("running once")
+        if( (new Date).getTime() > algoMaxEndTime){
+            console.log("ENding for time")
+            console.log("Ran times : " , i)
+            break;
+
+        }
         const randomizedInput = generateRandom1D(problem.numMachines, problem.numJobs)
         //console.log("randomized input " , randomizedInput)
         const problemCopy = Object.assign({}, problem)
