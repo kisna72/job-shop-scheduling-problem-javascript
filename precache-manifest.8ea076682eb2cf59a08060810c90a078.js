255facfaa7247febacb63551af3826c7237f3ac2
Krishna Regmi <kisna72@gmail.com>
2019-11-02 01:10:41 -0500

    Updates

--- precache-manifest.8ea076682eb2cf59a08060810c90a078.js
+++ precache-manifest.8ea076682eb2cf59a08060810c90a078.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5474a6a757eda4c007d6c30a1208280e",
+    "revision": "fb7f12ed33115cd1b64b3f757136883e",
     "url": "/job-shop-scheduling-problem-javascript/index.html"
   },
   {
-    "revision": "be49e764f562357f037c",
+    "revision": "2524c94c8b3ec5bd0bc7",
     "url": "/job-shop-scheduling-problem-javascript/static/css/2.c140e360.chunk.css"
   },
   {
-    "revision": "5273704955716f23948e",
+    "revision": "aae19fcf9e0900829330",
     "url": "/job-shop-scheduling-problem-javascript/static/css/main.61f05272.chunk.css"
   },
   {
-    "revision": "be49e764f562357f037c",
-    "url": "/job-shop-scheduling-problem-javascript/static/js/2.9ace5eca.chunk.js"
+    "revision": "2524c94c8b3ec5bd0bc7",
+    "url": "/job-shop-scheduling-problem-javascript/static/js/2.07dcba6b.chunk.js"
   },
   {
-    "revision": "5273704955716f23948e",
-    "url": "/job-shop-scheduling-problem-javascript/static/js/main.c143222a.chunk.js"
+    "revision": "aae19fcf9e0900829330",
+    "url": "/job-shop-scheduling-problem-javascript/static/js/main.c1cf9461.chunk.js"
   },
   {
     "revision": "78b01650961ca30522ca",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/job-shop-scheduling-problem-javascript/precache-manifest.2181db9065cfcbb167cc5db308b02e18.js"
+  "/job-shop-scheduling-problem-javascript/precache-manifest.8ea076682eb2cf59a08060810c90a078.js"
 );
 
 self.addEventListener('message', (event) => {
