75de25a9502caa005c8d8772334e19bfc68c23d9
Krishna Regmi <kisna72@gmail.com>
2020-04-27 20:26:07 -0500

    no use of spread

--- jssp-frontend/src/worker.js
+++ jssp-frontend/src/worker.js
@@ -13,13 +13,13 @@ const workercode = () => {
     //     };
     //     return __assign.apply(this, arguments);
     // };
-    var __spreadArrays = (this && this.__spreadArrays) || function () {
-        for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
-        for (var r = Array(s), k = 0, i = 0; i < il; i++)
-            for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
-                r[k] = a[j];
-        return r;
-    };
+    // var __spreadArrays = (this && this.__spreadArrays) || function () {
+    //     for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    //     for (var r = Array(s), k = 0, i = 0; i < il; i++)
+    //         for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+    //             r[k] = a[j];
+    //     return r;
+    // };
     var ResourceTypeEnum;
     (function (ResourceTypeEnum) {
         ResourceTypeEnum[ResourceTypeEnum["MACHINE"] = 0] = "MACHINE";
@@ -180,7 +180,7 @@ const workercode = () => {
                     ganttChartMachineMap.set(operation.machine, [jobId, startTime, endTime]);
                 }
                 else {
-                    ganttChartMachineMap.set(operation.machine, __spreadArrays(scheduleSoFar, [jobId, startTime, endTime]));
+                    ganttChartMachineMap.set(operation.machine, [...scheduleSoFar, jobId, startTime, endTime]);
                 }
                 return endTime;
             };
